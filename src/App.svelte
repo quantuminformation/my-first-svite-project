a0ef9adb626ca235d8c2e95e38f3226d9e37e581
Nikos Katsikanis <nkatsikanis@gmail.com>
2020-11-14 20:53:32 +0100

    intial

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,19 @@
 <script>
-  const world = 'world'; // edit world and save to see hmr update
+    import { Router, Route, link } from "svelte-navigator"
+    const world = "world" // edit world and save to see hmr update
+    const about = "about" // edit world and save to see hmr update
 </script>
 
 <style>
-  h1 {
-    color: orangered; /* change color an save to see hmr update */
-  }
+    h1 {
+        color: orangered; /* change color an save to see hmr update */
+    }
 </style>
 
 <h1>Hello {world}</h1>
-<p>Open App.svelte in your editor and change something to see HMR in action</p>
+<p>Open App.svelte in your editor and change something to see HMR in actiond</p>
+
+<Router>
+    <Route path="about" component="{about}" />
+</Router>
+s
